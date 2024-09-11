265999a95fd07a92f02270e249f4a9164236a305
mayrang <pkss0626@naver.com>
2024-09-12 00:55:01 +0900

    Fix: axios관련 설정 변경 및 baseurl 변경

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     port: 9999,
     open: true
   },
+  envDir: '../',
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
