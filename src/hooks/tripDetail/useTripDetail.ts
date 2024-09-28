48706a013d31910fa83a03234779af9fbde7797b
somm12 <sominan8744@gmail.com>
2024-09-28 15:28:15 +0900

    feat: 여행상세 화면 필요한 MSW 코드 작성, Hooks, store 코드 작성

---
+++ src/hooks/tripDetail/useTripDetail.ts
@@ -0,0 +1,44 @@
+import {
+  deleteTripDetail,
+  getTripDetail,
+  updateTripDetail
+} from '@/api/tripDetail'
+import { ITripDetail } from '@/model/tripDetail'
+import { authStore } from '@/store/client/authStore'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+const useTripDetail = (travelNumber: number) => {
+  const { userId, accessToken } = authStore()
+  const queryClient = useQueryClient()
+  const tripDetail = useQuery({
+    queryKey: ['tripDetail', travelNumber],
+    queryFn: () => getTripDetail(travelNumber, accessToken)
+    // enabled: !!travelNumber && !!accessToken
+  })
+
+  const { mutateAsync: updateTripDetailMutation } = useMutation({
+    mutationFn: (data: ITripDetail) => {
+      return updateTripDetail(travelNumber, data, accessToken)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tripDetail', travelNumber]
+      })
+    }
+  })
+
+  const { mutateAsync: deleteTripDetailMutation } = useMutation({
+    mutationFn: () => {
+      return deleteTripDetail(travelNumber, accessToken)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tripDetail', travelNumber]
+      })
+    }
+  })
+
+  return { tripDetail, updateTripDetailMutation, deleteTripDetailMutation }
+}
+
+export default useTripDetail
