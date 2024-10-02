be4024c6b98f0dc4185d734fd6d079db46a56ecb
mayrang <pkss0626@naver.com>
2024-10-02 15:49:40 +0900

    Feat: axios withcredential header로 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Credentials': true
   }
 })
