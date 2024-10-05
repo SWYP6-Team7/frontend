9e9f84ac63beac4e66e59bfce8b1b13c0eff564c
somm12 <sominan8744@gmail.com>
2024-10-05 22:22:54 +0900

    fix: 주최자 신청 목록 안보이는 문제 수정

--- src/hooks/useTripRecommendation.ts
+++ src/hooks/useTripRecommendation.ts
@@ -1,12 +1,12 @@
-import { getRecommendationTrips } from '@/api/home'
-import { useQuery } from '@tanstack/react-query'
+// import { getRecommendationTrips } from '@/api/home'
+// import { useQuery } from '@tanstack/react-query'
 
-export const useTripRecommendation = () => {
-  const data = useQuery({
-    queryKey: ['tripRecommendation'],
-    queryFn: () => {
-      return getRecommendationTrips()
-    }
-  })
-  return data
-}
+// export const useTripRecommendation = () => {
+//   const data = useQuery({
+//     queryKey: ['tripRecommendation'],
+//     queryFn: () => {
+//       return getRecommendationTrips()
+//     }
+//   })
+//   return data
+// }
