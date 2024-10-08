e699a77ab99ea4825649845336eb48065b8e3326
somm12 <sominan8744@gmail.com>
2024-10-08 19:53:10 +0900

    feat: splash 화면 구현

---
+++ src/pages/Splash.tsx
@@ -0,0 +1,47 @@
+import MoingFullLogo from '@/components/icons/MoingFullLogo'
+import { splashOnStore } from '@/store/client/splashOnOffStore'
+import { palette } from '@/styles/palette'
+import styled from '@emotion/styled'
+import React, { useEffect, useRef } from 'react'
+
+export default function Splash() {
+  const { splashOn, addSplashOn } = splashOnStore()
+  useEffect(() => {
+    const revisit = sessionStorage.getItem('revisit')
+
+    if (revisit === undefined || revisit === null) {
+      setTimeout(() => {
+        addSplashOn(false)
+      }, 2500)
+      sessionStorage.setItem('revisit', 'true')
+    } else if (revisit === 'true') {
+      addSplashOn(false)
+    }
+  }, [])
+
+  return (
+    <Container splashOn={splashOn}>
+      <LogoContainer>
+        <MoingFullLogo />
+      </LogoContainer>
+    </Container>
+  )
+}
+const Container = styled.div<{ splashOn: boolean }>`
+  /* display: ${props => (props.splashOn ? 'block' : 'none')}; */
+  position: absolute;
+  z-index: 2500;
+  height: 100svh;
+  width: 100%;
+  top: 0;
+  opacity: ${({ splashOn }) => (splashOn ? 1 : 0)};
+  transition: opacity 1s ease-in-out;
+  pointer-events: ${({ splashOn }) => (splashOn ? 'auto' : 'none')};
+  background-color: ${palette.keycolor};
+`
+const LogoContainer = styled.div`
+  display: flex;
+  height: 100svh;
+  justify-content: center;
+  align-items: center;
+`
