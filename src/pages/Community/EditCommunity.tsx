f5c418342361f7d793c506397860f0d410f6899d
mayrang <pkss0626@naver.com>
2024-10-17 11:23:52 +0900

    Fix: community edit 수정

--- src/pages/Community/EditCommunity.tsx
+++ src/pages/Community/EditCommunity.tsx
@@ -1,7 +1,7 @@
 import Communityform from '@/components/community/CommunityForm'
 
 const EditCommunity = () => {
-  return <Communityform />
+  return <Communityform isEdit={true} />
 }
 
 export default EditCommunity
