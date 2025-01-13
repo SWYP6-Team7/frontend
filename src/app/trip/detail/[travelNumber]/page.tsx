bd14121fa076c9b6f1f4e8e33162cc7e5c796a10
mayrang <pkss0626@naver.com>
2025-01-13 10:11:19 +0900

    Fix: detail page 프리페칭

--- src/app/trip/detail/[travelNumber]/page.tsx
+++ src/app/trip/detail/[travelNumber]/page.tsx
@@ -1,8 +1,25 @@
+import { getTripDetail } from "@/api/tripDetail";
 import TripDetail from "@/page/TripDetail/TripDetail";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import React from "react";
 
-const TripDetailPage = () => {
-  return <TripDetail />;
+const TripDetailPage = async ({ params }: { params: Promise<{ travelNumber: string }> }) => {
+  const travelNumber = (await params).travelNumber;
+  const queryClient = new QueryClient();
+
+  // 첫 페이지만 프리패치
+  await queryClient.prefetchQuery({
+    queryKey: ["tripDetail", travelNumber],
+    queryFn: () => getTripDetail(parseInt(travelNumber), null),
+  });
+  const data = queryClient.getQueryData(["tripDetail", travelNumber]);
+  console.log(data, "data");
+  const dehydratedstate = dehydrate(queryClient);
+  return (
+    <HydrationBoundary state={dehydratedstate}>
+      <TripDetail />
+    </HydrationBoundary>
+  );
 };
 
 export default TripDetailPage;
