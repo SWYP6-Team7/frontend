59ae56aa553dbf6c0436674f2c81326dd5688fb5
mayrang <pkss0626@naver.com>
2025-01-23 00:16:11 +0900

    Fix 돌아가기 버튼 a로 변경

--- src/components/errorHandling/NotFound.tsx
+++ src/components/errorHandling/NotFound.tsx
@@ -1,48 +1,30 @@
-'use client'
-import { palette } from '@/styles/palette'
-import styled from '@emotion/styled'
-import React from 'react'
-import InfoIcon from '../icons/InfoIcon'
-import { useRouter } from 'next/navigation'
+"use client";
+import { palette } from "@/styles/palette";
+import styled from "@emotion/styled";
+import React from "react";
+import InfoIcon from "../icons/InfoIcon";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <Container>
       <Body>
-        <InfoIcon
-          size={24}
-          color={palette.비강조2}
-        />
-        <img
-          src={'/images/noData.png'}
-          width={80}
-          height={80}
-          alt="No Data Logo Image"
-        />
+        <InfoIcon size={24} color={palette.비강조2} />
+        <img src={"/images/noData.png"} width={80} height={80} alt="No Data Logo Image" />
         <Title>페이지를 찾을 수 없습니다.</Title>
-        <BackButton onClick={() => router.replace('/')}>
+        <BackButton href="/">
           {/* back icon */}
-          <svg
-            width="6"
-            height="11"
-            viewBox="0 0 6 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M5 10L0.5 5.5L5 1"
-              stroke="#FEFEFE"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
+          <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 10L0.5 5.5L5 1" stroke="#FEFEFE" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
 
           <div>돌아가기</div>
         </BackButton>
       </Body>
     </Container>
-  )
-}
+  );
+};
 
 // 대중적인 mobile device는 430px 미만
 // 그렇기 때문에 440px 이상이면 모바일 환경이 아니라고 생각하고 max-width를 figma layout에 맞춤
@@ -67,9 +49,10 @@ const Body = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
-`
+`;
 // pc환경에서 화면을 가운데 정렬하기 위한 레이아웃 스타일
-const Container = styled.div`
+const Container = styled.a`
+  display: block;
   /* height는 홈화면 스크롤을 보기 위해서 auto로 잡아두기. width는 가로스크롤이 생겨서 auto로. */
   height: 100svh;
   width: 100svw;
@@ -84,7 +67,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Title = styled.h2`
   display: block;
@@ -93,7 +76,7 @@ const Title = styled.h2`
   line-height: 25.2px;
 
   text-align: center;
-`
+`;
 
 const BackButton = styled.button`
   padding: 9px 12px;
@@ -108,6 +91,6 @@ const BackButton = styled.button`
   border-radius: 20px;
   border: none;
   cursor: pointer;
-`
+`;
 
-export default NotFound
+export default NotFound;
