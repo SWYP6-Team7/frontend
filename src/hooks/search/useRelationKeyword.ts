1ab1f780a6bfd09bce003a3bc61077b42735908e
mayrang <pkss0626@naver.com>
2025-01-23 17:13:56 +0900

    Fix: 자동완성 에러 수정

--- src/hooks/search/useRelationKeyword.ts
+++ src/hooks/search/useRelationKeyword.ts
@@ -1,42 +1,39 @@
-'use client'
-import { getSearchRelation } from '@/api/search'
-import { authStore } from '@/store/client/authStore'
-import { useQuery } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+"use client";
+import { getSearchRelation } from "@/api/search";
+import { authStore } from "@/store/client/authStore";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 const useRelationKeyword = (keyword: string) => {
-  const [debouncedKeyword, setDebouncedKeyword] = useState(keyword)
-  const { accessToken } = authStore()
+  const [debouncedKeyword, setDebouncedKeyword] = useState(keyword);
+  const { accessToken } = authStore();
   const { data, isLoading, error } = useQuery({
-    queryKey: ['search', 'relation', debouncedKeyword],
-    queryFn: () => getSearchRelation(debouncedKeyword, accessToken),
-    enabled: keyword !== ''
-  })
+    queryKey: ["search", "relation", debouncedKeyword],
+    queryFn: () => getSearchRelation(debouncedKeyword.replace("+", ""), accessToken),
+    enabled: keyword !== "",
+  });
 
   useEffect(() => {
     if (keyword.length === 1) {
-      setDebouncedKeyword(keyword)
+      setDebouncedKeyword(keyword);
     }
     const handler = setTimeout(() => {
       if (keyword.length > 0) {
         if (
-          (keyword.charCodeAt(keyword.length - 1) >= 0xac00 &&
-            keyword.charCodeAt(keyword.length - 1) <= 0xd7a3) ||
-          (keyword.charCodeAt(keyword.length - 1) >= 65 &&
-            keyword.charCodeAt(keyword.length - 1) <= 90) ||
-          (keyword.charCodeAt(keyword.length - 1) >= 97 &&
-            keyword.charCodeAt(keyword.length - 1) <= 122)
+          (keyword.charCodeAt(keyword.length - 1) >= 0xac00 && keyword.charCodeAt(keyword.length - 1) <= 0xd7a3) ||
+          (keyword.charCodeAt(keyword.length - 1) >= 65 && keyword.charCodeAt(keyword.length - 1) <= 90) ||
+          (keyword.charCodeAt(keyword.length - 1) >= 97 && keyword.charCodeAt(keyword.length - 1) <= 122)
         ) {
-          setDebouncedKeyword(keyword)
+          setDebouncedKeyword(keyword);
         }
       }
-    }, 300)
+    }, 300);
     return () => {
-      clearTimeout(handler)
-    }
-  }, [keyword])
+      clearTimeout(handler);
+    };
+  }, [keyword]);
 
-  return { data, isLoading, error }
-}
+  return { data, isLoading, error };
+};
 
-export default useRelationKeyword
+export default useRelationKeyword;
