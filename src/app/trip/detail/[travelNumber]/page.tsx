b6dbda2bbe6a78266c5262e29b9adca4366189c7
mayrang <pkss0626@naver.com>
2025-01-24 13:24:04 +0900

    Fix: trip detail prefetch 주석 처리

--- src/app/trip/detail/[travelNumber]/page.tsx
+++ src/app/trip/detail/[travelNumber]/page.tsx
@@ -4,22 +4,22 @@ import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query
 import React from "react";
 
 const TripDetailPage = async ({ params }: { params: { travelNumber: string } }) => {
-  const travelNumber = params.travelNumber;
-  const queryClient = new QueryClient();
+  // const travelNumber = params.travelNumber;
+  // const queryClient = new QueryClient();
 
-  // 첫 페이지만 프리패치
-  await queryClient.prefetchQuery({
-    queryKey: ["tripDetail", travelNumber],
-    queryFn: () => {
-      const data = getTripDetail(parseInt(travelNumber), null);
-      return JSON.parse(JSON.stringify(data));
-    },
-  });
-  const dehydratedstate = dehydrate(queryClient);
+  // // 첫 페이지만 프리패치
+  // await queryClient.prefetchQuery({
+  //   queryKey: ["tripDetail", travelNumber],
+  //   queryFn: () => {
+  //     const data = getTripDetail(parseInt(travelNumber), null);
+  //     return JSON.parse(JSON.stringify(data));
+  //   },
+  // });
+  // const dehydratedstate = dehydrate(queryClient);
   return (
-    <HydrationBoundary state={dehydratedstate}>
-      <TripDetail />
-    </HydrationBoundary>
+    // <HydrationBoundary state={dehydratedstate}>
+    <TripDetail />
+    //</HydrationBoundary>
   );
 };
 
