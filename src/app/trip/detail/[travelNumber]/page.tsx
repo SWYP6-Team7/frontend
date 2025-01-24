141b705c06d6f08ca095b0758cdbf93633e091f6
mayrang <pkss0626@naver.com>
2025-01-24 13:54:36 +0900

    Fix: 여행 상세보기 rsc

--- src/app/trip/detail/[travelNumber]/page.tsx
+++ src/app/trip/detail/[travelNumber]/page.tsx
@@ -2,6 +2,23 @@ import { getTripDetail } from "@/api/tripDetail";
 import TripDetail from "@/page/TripDetail/TripDetail";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import React from "react";
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ travelNumber: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  const travelNumber = (await params).travelNumber;
+
+  // fetch data
+  const tripDetail = await getTripDetail(parseInt(travelNumber), null);
+
+  return {
+    title: tripDetail.data?.data?.title || "여행 상세보기",
+  };
+}
 
 const TripDetailPage = async ({ params }: { params: { travelNumber: string } }) => {
   const travelNumber = params.travelNumber;
@@ -9,9 +26,9 @@ const TripDetailPage = async ({ params }: { params: { travelNumber: string } }) 
 
   await queryClient.prefetchQuery({
     queryKey: ["tripDetail", travelNumber],
-    queryFn: () => {
-      const data = getTripDetail(parseInt(travelNumber), null);
-      return data;
+    queryFn: async () => {
+      const data = await getTripDetail(parseInt(travelNumber), null);
+      return JSON.parse(JSON.stringify(data));
     },
   });
   const dehydratedstate = dehydrate(queryClient);
