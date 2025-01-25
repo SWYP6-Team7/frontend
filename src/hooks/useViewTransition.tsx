4a6211f8d03b33c70c801e941a06b47b1390208c
mayrang <pkss0626@naver.com>
2025-01-25 23:46:00 +0900

    Fix: 계속 변경

--- src/hooks/useViewTransition.tsx
+++ src/hooks/useViewTransition.tsx
@@ -5,13 +5,18 @@ const useViewTransition = () => {
   const router = useRouter();
 
   const navigateWithTransition = (to: string) => {
-    if (!(document as any).startViewTransition) {
-      router.push(to);
-      return;
-    }
+    const startTransition = (callback: () => void) => {
+      if ((document as any).startViewTransition) {
+        (document as any).startViewTransition(callback);
+      } else {
+        callback();
+      }
+    };
 
-    (document as any).startViewTransition(async () => {
-      router.push(to);
+    startTransition(() => {
+      setTimeout(() => {
+        router.push(to);
+      }, 0); // 브라우저에서 Transition API와 라우팅의 타이밍을 맞추기 위한 지연 처리
     });
   };
 
