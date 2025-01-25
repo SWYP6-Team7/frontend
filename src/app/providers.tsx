6cce451a7c29dbaa4ebd1df833f627aa637d05b8
mayrang <pkss0626@naver.com>
2025-01-26 00:17:01 +0900

    라이브러리 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,15 +4,18 @@ import ErrorCatcher from "@/context/ErrorCatcher";
 import { ReactNode } from "react";
 import RootStyleRegistry from "./RootStyleRegistry";
 import { GlobalErrorBoundary } from "@/components/errorHandling/GlobalErrorBoundary";
+import { ViewTransitions } from "next-view-transitions";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <GlobalErrorBoundary>
-      <QueryClientBoundary>
-        <ErrorCatcher />
+      <ViewTransitions>
+        <QueryClientBoundary>
+          <ErrorCatcher />
 
-        <RootStyleRegistry> {children}</RootStyleRegistry>
-      </QueryClientBoundary>
+          <RootStyleRegistry> {children}</RootStyleRegistry>
+        </QueryClientBoundary>
+      </ViewTransitions>
     </GlobalErrorBoundary>
   );
 }
--- src/hooks/useViewTransition.tsx
+++ src/hooks/useViewTransition.tsx
@@ -1,25 +1,17 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useTransitionRouter } from "next-view-transitions";
 
 const useViewTransition = () => {
-  const router = useRouter();
+  const router = useTransitionRouter();
 
   const navigateWithTransition = (to: string) => {
     if (!(document as any).startViewTransition) {
-      // startViewTransition 미지원 브라우저 대응
       router.push(to);
       return;
     }
 
-    // startViewTransition 지원 브라우저
-    (document as any).startViewTransition(() => {
-      // 다음 프레임에서 DOM 업데이트 보장
-      return new Promise((resolve) => {
-        requestAnimationFrame(() => {
-          resolve(router.push(to)); // DOM이 안정된 후에 라우팅
-        });
-      });
-    });
+    router.push(to);
   };
 
   return navigateWithTransition;
