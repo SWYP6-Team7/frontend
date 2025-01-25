91beb198e32d7942a80e39a650b283af697ca111
mayrang <pkss0626@naver.com>
2025-01-25 22:54:38 +0900

    Fix: navigationWithTransition 수정

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,30 +1,27 @@
-'use client'
-import useViewTransition from '@/hooks/useViewTransition'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import useViewTransition from "@/hooks/useViewTransition";
+import Link from "next/link";
+import React from "react";
 
 type CustomLinkProps = {
-  to: string
-  children: React.ReactNode
-}
+  to: string;
+  children: React.ReactNode;
+};
 
 const CustomLink = ({ to, children, ...props }: CustomLinkProps) => {
-  const navigateWithTransition = useViewTransition()
+  const navigateWithTransition = useViewTransition();
 
   const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault()
-    document.documentElement.style.viewTransitionName = 'forward'
-    navigateWithTransition(to)
-  }
+    e.preventDefault();
+    document.documentElement.style.viewTransitionName = "forward";
+    navigateWithTransition(to);
+  };
 
   return (
-    <Link
-      href={to}
-      onClick={handleClick}
-      {...props}>
+    <Link href={to} onClick={handleClick} {...props}>
       {children}
     </Link>
-  )
-}
+  );
+};
 
-export default CustomLink
+export default CustomLink;
--- src/hooks/useViewTransition.tsx
+++ src/hooks/useViewTransition.tsx
@@ -4,16 +4,16 @@ const useViewTransition = () => {
   const router = useRouter();
 
   const navigateWithTransition = (to: string) => {
-    // Check browser support for View Transitions
     if (!(document as any).startViewTransition) {
       router.push(to);
       return;
     }
 
-    // Start view transition
-    (document as any).startViewTransition(() => {
-      router.push(to);
-    });
+    // 페이지 전환을 먼저 트리거
+    router.push(to);
+
+    // 애니메이션은 다음 렌더링 사이클에서 시작
+    (document as any).startViewTransition(() => {});
   };
 
   return navigateWithTransition;
