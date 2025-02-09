1801dfa983a2eb13b68c80df4ce921204479cc66
mayrang <pkss0626@naver.com>
2025-02-10 01:41:28 +0900

    Fix: query provider 변경

--- src/context/QueryClientBoundary.tsx
+++ src/context/QueryClientBoundary.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState, useEffect } from "react";
 import { errorStore } from "@/store/client/errorStore";
 import {
   Mutation,
@@ -9,12 +8,11 @@ import {
   QueryClient,
   QueryClientProvider,
   defaultShouldDehydrateQuery,
-  isServer,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-export default function QueryClientBoundary({ children }: React.PropsWithChildren) {
-  const { updateError, setIsMutationError } = errorStore();
-  const queryClient = new QueryClient({
+
+function makeQueryClient(updateError: (error: Error) => void, setIsMutationError: (isMutation: boolean) => void) {
+  return new QueryClient({
     queryCache: new QueryCache({
       onError: (error: Error, query: Query<unknown, unknown, unknown, readonly unknown[]>) => {
         const queryKey = query?.queryKey;
@@ -54,12 +52,35 @@ export default function QueryClientBoundary({ children }: React.PropsWithChildre
         refetchOnMount: true,
         staleTime: 60 * 1000,
       },
+
       dehydrate: {
         // include pending queries in dehydration
         shouldDehydrateQuery: (query) => defaultShouldDehydrateQuery(query) || query.state.status === "pending",
       },
     },
   }); // QueryClient 상태 초기화
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+function getQueryClient(updateError: (error: Error) => void, setIsMutationError: (isMutation: boolean) => void) {
+  if (typeof window === "undefined") {
+    // Server일 경우
+    // 매번 새로운 queryClient를 만든다.
+    return makeQueryClient(updateError, setIsMutationError);
+  } else {
+    // Browser일 경우
+    // queryClient가 존재하지 않을 경우에만 새로운 queryClient를 만든다.
+    // React가 새 Client를 만들게 하기 위해 중요하다.
+    if (!browserQueryClient) browserQueryClient = makeQueryClient(updateError, setIsMutationError);
+    return browserQueryClient;
+  }
+}
+
+export const queryClient = new QueryClient();
+export default function QueryClientBoundary({ children }: React.PropsWithChildren) {
+  const { updateError, setIsMutationError } = errorStore();
+
+  const queryClient = getQueryClient(updateError, setIsMutationError);
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
