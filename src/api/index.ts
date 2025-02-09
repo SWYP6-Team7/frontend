84b112833f86cc500401c5ec3b06979e60957a67
mayrang <pkss0626@naver.com>
2025-02-10 03:18:20 +0900

    Fix: live 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_LIVE_BASE_URL || process.env.API_LIVE_BASE_URL,
   timeout: 5000,
 
   headers: {
