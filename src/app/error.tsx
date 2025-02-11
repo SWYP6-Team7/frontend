a15f9b97c9d8206a17c419f4acdd9c94cb64ba1a
mayrang <pkss0626@naver.com>
2025-02-12 03:29:05 +0900

    Fix: 에러 페이지 삭제해보기

--- src/app/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Fallback from "@/components/errorHandling/Fallback";
-import NotFound from "@/components/errorHandling/NotFound";
-import React from "react";
-
-const ErrorPage = () => {
-  return <Fallback />;
-};
-export default ErrorPage;
