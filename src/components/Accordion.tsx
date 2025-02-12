a0e2cb5de71262529f9c085e93855cdf87afe505
mayrang <pkss0626@naver.com>
2025-02-12 21:19:44 +0900

    Fix: 아코디언 애니메이션 변경

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,104 +1,84 @@
-'use client'
-import styled from '@emotion/styled'
-import React, { useState } from 'react'
-import SelectArrow from './icons/SelectArrow'
-import { palette } from '@/styles/palette'
-import Vector from './icons/Vector'
-import { usePathname } from 'next/navigation'
+"use client";
+import styled from "@emotion/styled";
+import React, { useState } from "react";
+import SelectArrow from "./icons/SelectArrow";
+import { palette } from "@/styles/palette";
+import Vector from "./icons/Vector";
+import { usePathname } from "next/navigation";
 
 const Accordion = ({
   id,
   title,
   children,
   initialChecked,
-  count
+  count,
 }: {
-  id: string
-  count: number
-  title: string
-  children: React.ReactNode
-  initialChecked: boolean
+  id: string;
+  count: number;
+  title: string;
+  children: React.ReactNode;
+  initialChecked: boolean;
 }) => {
-  const [isChecked, setIsChecked] = useState(initialChecked)
-  const pathname = usePathname()
-  const isCreateTripPage = pathname === '/createTripDetail'
-  const isEditTripPage = pathname?.startsWith('/trip/edit')
+  const [isChecked, setIsChecked] = useState(initialChecked);
+  const pathname = usePathname();
+  const isCreateTripPage = pathname === "/createTripDetail";
+  const isEditTripPage = pathname?.startsWith("/trip/edit");
 
   return (
     <List>
       <input
         type="checkbox"
         id={id}
-        style={{ display: 'none' }}
+        style={{ display: "none" }}
         checked={isChecked}
         onChange={() => setIsChecked(!isChecked)}
       />
-      <Tab
-        htmlFor={id}
-        isCreateTripPage={Boolean(
-          (isCreateTripPage || isEditTripPage) && isChecked
-        )}>
+      <Tab htmlFor={id} isCreateTripPage={Boolean((isCreateTripPage || isEditTripPage) && isChecked)}>
         <TitleContainer>
           {isCreateTripPage ? (
-            <TitleTextCreateTrip
-              isCreateTripPage={isCreateTripPage || isEditTripPage}>
-              {title}
-            </TitleTextCreateTrip>
+            <TitleTextCreateTrip isCreateTripPage={isCreateTripPage || isEditTripPage}>{title}</TitleTextCreateTrip>
           ) : (
             <div>{title}</div>
           )}
 
-          {count > 0 && (
-            <Count
-              isCreateTripPage={isCreateTripPage || Boolean(isEditTripPage)}>
-              {count}
-            </Count>
-          )}
+          {count > 0 && <Count isCreateTripPage={isCreateTripPage || Boolean(isEditTripPage)}>{count}</Count>}
         </TitleContainer>
-        <div style={{ transform: isChecked ? 'rotate(180deg)' : 'rotate(0)' }}>
-          {isCreateTripPage ? (
-            <Vector stroke={palette.비강조} />
-          ) : (
-            <SelectArrow
-              width={12}
-              height={6}
-            />
-          )}
+        <div style={{ transform: isChecked ? "rotate(180deg)" : "rotate(0)" }}>
+          {isCreateTripPage ? <Vector stroke={palette.비강조} /> : <SelectArrow width={12} height={6} />}
         </div>
       </Tab>
-      <Content
-        checked={isChecked}
-        isCreateTripPage={isCreateTripPage || Boolean(isEditTripPage)}>
+      <Content checked={isChecked} isCreateTripPage={isCreateTripPage || Boolean(isEditTripPage)}>
         {children}
       </Content>
       {isCreateTripPage && isChecked && (
         <div
           style={{
-            marginTop: '24px',
-            height: '1px',
-            border: '0.5px solid rgba(240, 240, 240, 1)'
-          }}></div>
+            marginTop: "24px",
+            height: "1px",
+            border: "0.5px solid rgba(240, 240, 240, 1)",
+          }}
+        ></div>
       )}
     </List>
-  )
-}
+  );
+};
 
 const TitleContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 8px;
-`
+`;
 const TitleTextCreateTrip = styled.div<{ isCreateTripPage: boolean }>`
   font-size: 18px;
   font-weight: 600;
-  line-height: ${props => (props.isCreateTripPage ? '29.2px' : '25.2px')};
+  line-height: ${(props) => (props.isCreateTripPage ? "29.2px" : "25.2px")};
   text-align: left;
   color: ${palette.기본};
   height: 25px;
   padding: 0px 6px;
   gap: 8px;
   opacity: 0px;
-`
+`;
 const Count = styled.div<{ isCreateTripPage: boolean }>`
   width: 16px;
   height: 16px;
@@ -114,11 +94,11 @@ const Count = styled.div<{ isCreateTripPage: boolean }>`
   font-size: 12px;
   font-weight: 600;
 
-  line-height: ${props => (props.isCreateTripPage ? '0' : '14.32px')};
+  line-height: ${(props) => (props.isCreateTripPage ? "0" : "14.32px")};
   color: ${palette.비강조4};
 
   opacity: 0px;
-`
+`;
 
 const Tab = styled.label<{ isCreateTripPage: boolean }>`
   display: flex;
@@ -129,30 +109,26 @@ const Tab = styled.label<{ isCreateTripPage: boolean }>`
 
   align-items: center;
   justify-content: space-between;
-  border-bottom: ${props =>
-    props.isCreateTripPage ? 'none' : '1px solid rgba(240, 240, 240, 1)'};
+  border-bottom: ${(props) => (props.isCreateTripPage ? "none" : "1px solid rgba(240, 240, 240, 1)")};
   padding: 2.3svh 0;
   cursor: pointer;
-`
+`;
 
 const Content = styled.div<{ checked: boolean; isCreateTripPage: boolean }>`
-  max-height: ${props =>
-    props.checked ? (props.isCreateTripPage ? '55svh' : '25svh') : '0'};
+  max-height: ${(props) => (props.checked ? (props.isCreateTripPage ? "55svh" : "25svh") : "0")};
 
-  padding: ${props => (props.checked ? '1.7svh' : '0 1.7svh')};
-  opacity: ${props => (props.checked ? '1' : '0 ')};
+  padding: ${(props) => (props.checked ? "1.7svh" : "0 1.7svh")};
+  opacity: ${(props) => (props.checked ? "1" : "0 ")};
 
-  transform: ${props => (props.checked ? 'translateY(0)' : 'translateY(20px)')};
+  transform: ${(props) => (props.checked ? "translateY(0)" : "translateY(20px)")};
   transition:
     max-height 0.3s ease,
-    opacity 0.3s ease,
-    transform 0.7s ease,
     padding 0.3s ease;
-`
+`;
 
 const List = styled.li`
   overflow: hidden;
   list-style: none;
-`
+`;
 
-export default Accordion
+export default Accordion;
