d7472b95a51588435d721e71d7c45281edda4474
mayrang <pkss0626@naver.com>
2025-02-14 06:52:54 +0900

    Fix: 다른 방법2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <meta http-equiv="refresh" content={`20; url=${process.env.FRONT_URL}`}></meta>
       <body>
         <Providers>
           <Layout>{children}</Layout>
