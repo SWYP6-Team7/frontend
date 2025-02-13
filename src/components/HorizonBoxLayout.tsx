da405313099bb5cadadf5e4cf2e46afdcf393463
mayrang <pkss0626@naver.com>
2025-02-14 04:33:24 +0900

    Fix: 콘솔찍기

--- src/components/HorizonBoxLayout.tsx
+++ src/components/HorizonBoxLayout.tsx
@@ -69,6 +69,7 @@ const HorizonBoxLayout = ({
 
   const [tagsCount, setTagsCount] = useState(cutTags);
   useLayoutEffect(() => {
+    console.log("test", tagsCount, cutTags, tagRef.current);
     if (tagRef.current && tagRef?.current.getBoundingClientRect().height >= 24) {
       console.log(tagsCount, "tag");
       setTagsCount((prev) => (prev.length > 0 ? prev.slice(0, 1) : []));
