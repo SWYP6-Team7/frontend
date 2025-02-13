e02eae317c0c18fc016ec3317e9fa72b4c2f51a3
mayrang <pkss0626@naver.com>
2025-02-14 04:50:00 +0900

    Fix: forwardref로 변경

--- src/components/designSystem/tag/BoxLayoutTag.tsx
+++ src/components/designSystem/tag/BoxLayoutTag.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { palette } from "@/styles/palette";
 import styled from "@emotion/styled";
-import { RefObject } from "react";
+import { forwardRef, RefObject } from "react";
 
 interface BoxLayoutTagProps {
   text: React.ReactNode;
   size?: "small" | "medium" | "large";
-  ref?: RefObject<HTMLDivElement | null>;
+
   addStyle?: {
     backgroundColor?: string;
     color?: string;
@@ -18,50 +18,55 @@ interface BoxLayoutTagProps {
     fontWeight?: string;
   };
 }
-const BoxLayoutTag = ({
-  text,
-  size,
-  ref,
-  addStyle = {
-    backgroundColor: `${palette.비강조4}`,
-    padding: "4px 10px 4px 10px",
-    color: `${palette.비강조}`,
-    borderRadius: "20px",
-    fontSize: "12px",
-    fontWeight: "600",
-  },
-}: BoxLayoutTagProps) => {
-  const style = size
-    ? size === "large"
-      ? {
-          ...addStyle,
-          padding: "14px 24px",
-          fontSize: "16px",
-          height: "48px",
-          borderRadius: "30px",
-        }
-      : size === "medium"
+const BoxLayoutTag = forwardRef(
+  (
+    {
+      text,
+      size,
+
+      addStyle = {
+        backgroundColor: `${palette.비강조4}`,
+        padding: "4px 10px 4px 10px",
+        color: `${palette.비강조}`,
+        borderRadius: "20px",
+        fontSize: "12px",
+        fontWeight: "600",
+      },
+    }: BoxLayoutTagProps,
+    ref
+  ) => {
+    const style = size
+      ? size === "large"
         ? {
             ...addStyle,
-            padding: "10px 20px",
+            padding: "14px 24px",
             fontSize: "16px",
-            height: "42px",
+            height: "48px",
             borderRadius: "30px",
           }
-        : {
-            ...addStyle,
-            padding: "8px 14px",
-            fontSize: "14px",
-            height: "33px",
-            borderRadius: "16px",
-          }
-    : addStyle;
-  return (
-    <Tag ref={ref} style={style}>
-      {text}
-    </Tag>
-  );
-};
+        : size === "medium"
+          ? {
+              ...addStyle,
+              padding: "10px 20px",
+              fontSize: "16px",
+              height: "42px",
+              borderRadius: "30px",
+            }
+          : {
+              ...addStyle,
+              padding: "8px 14px",
+              fontSize: "14px",
+              height: "33px",
+              borderRadius: "16px",
+            }
+      : addStyle;
+    return (
+      <Tag ref={ref} style={style}>
+        {text}
+      </Tag>
+    );
+  }
+);
 
 const Tag = styled.div`
   display: flex;
