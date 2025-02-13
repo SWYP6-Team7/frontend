b310ea166747286b7dc450c37fb684d199a57556
mayrang <pkss0626@naver.com>
2025-02-13 13:23:50 +0900

    Fix: useViewTransition 변경

--- src/hooks/useViewTransition.tsx
+++ src/hooks/useViewTransition.tsx
@@ -1,17 +1,45 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
 import { useTransitionRouter } from "next-view-transitions";
 
 const useViewTransition = () => {
   const router = useTransitionRouter();
+  const swipeDetectedRef = useRef(false);
+
+  useEffect(() => {
+    const handleTouchStart = (event: TouchEvent) => {
+      if (event.touches[0].clientX < 50) {
+        swipeDetectedRef.current = true;
+      }
+    };
+
+    const handleTouchEnd = () => {
+      swipeDetectedRef.current = false;
+    };
+
+    window.addEventListener("touchstart", handleTouchStart);
+    window.addEventListener("touchend", handleTouchEnd);
+
+    return () => {
+      window.removeEventListener("touchstart", handleTouchStart);
+      window.removeEventListener("touchend", handleTouchEnd);
+    };
+  }, []);
 
   const navigateWithTransition = (to: string) => {
+    if (swipeDetectedRef.current) {
+      router.push(to);
+      return;
+    }
+
     if (!(document as any).startViewTransition) {
       router.push(to);
       return;
     }
 
-    router.push(to);
+    document.startViewTransition(() => {
+      router.push(to);
+    });
   };
 
   return navigateWithTransition;
