316a48ec9bf3e1873c3a0e8a363ae51fbb796ba3
mayrang <pkss0626@naver.com>
2025-02-15 21:15:04 +0900

    Fix: 클러스터링 추가해보기

--- src/components/map/GoogleMap.tsx
+++ src/components/map/GoogleMap.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { AdvancedMarker, APIProvider, Map, MapCameraChangedEvent, Pin } from "@vis.gl/react-google-maps";
-import React from "react";
+import { AdvancedMarker, APIProvider, Map, MapCameraChangedEvent, Pin, useMap } from "@vis.gl/react-google-maps";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import type { Marker } from "@googlemaps/markerclusterer";
 
 type Poi = { key: string; location: google.maps.LatLngLiteral };
 const locations: Poi[] = [
@@ -22,10 +24,54 @@ const locations: Poi[] = [
 ];
 
 const PoiMarkers = (props: { pois: Poi[] }) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+  const clusterer = useRef<MarkerClusterer | null>(null);
+
+  // Initialize MarkerClusterer, if the map has changed
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map });
+    }
+  }, [map]);
+
+  // Update markers, if the markers array has changed
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+  const handleClick = (ev: google.maps.MapMouseEvent) => {
+    if (!map) return;
+    if (!ev.latLng) return;
+    console.log("marker clicked:", ev.latLng.toString());
+    map.panTo(ev.latLng);
+  };
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers((prev) => {
+      if (marker) {
+        return { ...prev, [key]: marker };
+      } else {
+        const newMarkers = { ...prev };
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
   return (
     <>
       {props.pois.map((poi: Poi) => (
-        <AdvancedMarker key={poi.key} position={poi.location}>
+        <AdvancedMarker
+          clickable={true}
+          onClick={handleClick}
+          key={poi.key}
+          position={poi.location}
+          ref={(marker) => setMarkerRef(marker, poi.key)}
+        >
           <Pin background={"#FBBC04"} glyphColor={"#000"} borderColor={"#000"} />
         </AdvancedMarker>
       ))}
@@ -35,21 +81,23 @@ const PoiMarkers = (props: { pois: Poi[] }) => {
 
 const GoogleMap = () => {
   return (
-    <APIProvider
-      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API || ""}
-      onLoad={() => console.log("Maps API has loaded.")}
-    >
-      <Map
-        defaultZoom={13}
-        mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_ID || ""}
-        defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
-        onCameraChanged={(ev: MapCameraChangedEvent) =>
-          console.log("camera changed:", ev.detail.center, "zoom:", ev.detail.zoom)
-        }
+    <div style={{ width: "100%", height: 300 }}>
+      <APIProvider
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API || ""}
+        onLoad={() => console.log("Maps API has loaded.")}
       >
-        <PoiMarkers pois={locations} />
-      </Map>
-    </APIProvider>
+        <Map
+          defaultZoom={13}
+          mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_ID || ""}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log("camera changed:", ev.detail.center, "zoom:", ev.detail.zoom)
+          }
+        >
+          <PoiMarkers pois={locations} />
+        </Map>
+      </APIProvider>
+    </div>
   );
 };
 
