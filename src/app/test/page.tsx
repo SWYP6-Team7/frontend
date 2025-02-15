15b4c467ab39d7ff055a4d6f6a29fc5b9fe573db
mayrang <pkss0626@naver.com>
2025-02-15 21:06:04 +0900

    Fix: 구글맵 적용해보기

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import GoogleMap from "@/components/map/GoogleMap";
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <div>
+      <GoogleMap />
+    </div>
+  );
+};
+
+export default TestPage;
