2b627ee9bee8b0c82f585736daa6b41fd27102f2
mayrang <pkss0626@naver.com>
2025-02-20 23:36:12 +0900

    Fix: api profile me 대응

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -29,7 +29,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   const isCommunityDetail = pathname?.startsWith("/community/detail");
 
-  const myPageData: ImyPage = (data as any)?.data;
+  const myPageData: ImyPage = data as any;
   const profileImg: IProfileImg = profileImage as IProfileImg;
   console.log(profileImg, profileImage, "프로필 이미지 get");
   useEffect(() => {
