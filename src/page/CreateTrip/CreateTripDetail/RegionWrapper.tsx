50a31e86cbca0d7295f4d3edba296c9bcdd430bf
mayrang <pkss0626@naver.com>
2025-03-14 22:54:37 +0900

    Fix: 콘솔찍기

--- src/page/CreateTrip/CreateTripDetail/RegionWrapper.tsx
+++ src/page/CreateTrip/CreateTripDetail/RegionWrapper.tsx
@@ -5,12 +5,7 @@ import { palette } from "@/styles/palette";
 import styled from "@emotion/styled";
 import React, { useCallback, useEffect, useState } from "react";
 
-import {
-  APIProvider,
-  Map,
-  useMap,
-  useMapsLibrary,
-} from "@vis.gl/react-google-maps";
+import { APIProvider, Map, useMap, useMapsLibrary } from "@vis.gl/react-google-maps";
 import { createTripStore } from "@/store/client/createTripStore";
 import RegionModal from "@/components/RegionModal";
 
@@ -26,13 +21,7 @@ const RegionWrapper = ({
   isDetail = false,
 }: {
   locationName: { locationName: string; mapType: "google" | "kakao" };
-  addLocationName: ({
-    locationName,
-    mapType,
-  }: {
-    locationName: string;
-    mapType: "google" | "kakao";
-  }) => void;
+  addLocationName: ({ locationName, mapType }: { locationName: string; mapType: "google" | "kakao" }) => void;
   addInitGeometry: (obj: { lat: number; lng: number } | null) => void;
   location?: string;
   isDetail?: boolean;
@@ -45,6 +34,7 @@ const RegionWrapper = ({
   const locationNameStr = location ?? locationName.locationName;
 
   const fetchPlaceInfo = async () => {
+    console.log(map, placesLib, "result");
     if (!map || !placesLib) return;
     const { PlacesService } = placesLib as google.maps.PlacesLibrary;
     const service = new PlacesService(map);
@@ -57,10 +47,7 @@ const RegionWrapper = ({
     service.findPlaceFromQuery(request, (results, status) => {
       if (status === google.maps.places.PlacesServiceStatus.OK && results) {
         console.log(results, "result");
-        if (
-          results[0].geometry?.location?.lat() &&
-          results[0].geometry?.location?.lng()
-        ) {
+        if (results[0].geometry?.location?.lat() && results[0].geometry?.location?.lng()) {
           addInitGeometry({
             lat: results[0].geometry?.location?.lat(),
             lng: results[0].geometry?.location?.lng(),
@@ -69,9 +56,7 @@ const RegionWrapper = ({
         // 결과 처리
 
         if (results[0]) {
-          const parts = results[0].formatted_address
-            ?.split(",")
-            .map((part) => part.trim());
+          const parts = results[0].formatted_address?.split(",").map((part) => part.trim());
           setRegionInfo({
             country: parts ? parts[parts.length - 1] : null,
             adminArea: parts ? parts[parts.length - 2] : null,
@@ -95,10 +80,7 @@ const RegionWrapper = ({
         console.log(result, "result");
 
         // 정상적으로 검색이 완료됐으면
-        if (
-          status === window.kakao.maps.services.Status.OK &&
-          result.length > 0
-        ) {
+        if (status === window.kakao.maps.services.Status.OK && result.length > 0) {
           if (result[0].x && result[0].y) {
             addInitGeometry({
               lat: Number(result[0].y),
