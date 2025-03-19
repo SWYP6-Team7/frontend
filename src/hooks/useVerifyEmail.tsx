37134cc433c72c55080d33d70fa3b3d33169ba19
mayrang <pkss0626@naver.com>
2025-03-20 02:06:32 +0900

    Fix: reponse -> response.data

--- src/hooks/useVerifyEmail.tsx
+++ src/hooks/useVerifyEmail.tsx
@@ -18,7 +18,7 @@ const useVerifyEmail = () => {
       if (!response.data?.success) {
         throw new Error(response.data?.error.reason);
       }
-      return response;
+      return response?.data;
     },
     onSuccess: (data: any) => {
       console.log(data, "data");
@@ -47,7 +47,7 @@ const useVerifyEmail = () => {
       if (!response.data?.success) {
         throw new RequestError(response.data?.error.reason);
       }
-      return response;
+      return response?.data;
     },
     mutationKey: ["verifyEmailCode"],
     onSuccess: (data: any) => {
