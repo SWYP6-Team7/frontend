27c200017166f622e231b121c3925a2ed8678f56
mayrang <pkss0626@naver.com>
2025-03-22 00:52:06 +0900

    Feat: 신고 소명 부분 추가

---
+++ src/app/explanation/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExplanationPage = () => {
+  return <div></div>;
+};
+
+export default ExplanationPage;
--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -22,6 +22,8 @@ const NotificationItem = ({ data }: NotificationItemProps) => {
       router.push(`/trip/comment/${data.travelNumber}`);
     } else if (data.title === "커뮤니티") {
       router.push(`/community/detail/${data.travelNumber}`);
+    } else if (data?.title === "신고 접수 경고") {
+      return;
     } else {
       router.push(`/trip/detail/${data.travelNumber}`);
       setTravelDetail("/notification");
@@ -30,6 +32,20 @@ const NotificationItem = ({ data }: NotificationItemProps) => {
   return (
     <Container onClick={onclickLink}>
       <TopContainer>
+        {data?.title === "신고 접수 경고" && (
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="24" height="24" rx="12" fill="#3E8D00" />
+            <path
+              d="M11.668 6.33333C11.668 6.14924 11.8172 6 12.0013 6V6C12.1854 6 12.3346 6.14924 12.3346 6.33333V12.8667C12.3346 13.0508 12.1854 13.2 12.0013 13.2V13.2C11.8172 13.2 11.668 13.0508 11.668 12.8667V6.33333Z"
+              stroke="#FDFDFD"
+              strokeWidth="2"
+            />
+            <path
+              d="M10.5 17.5C10.5 16.6716 11.1716 16 12 16C12.8284 16 13.5 16.6716 13.5 17.5C13.5 18.3284 12.8284 19 12 19C11.1716 19 10.5 18.3284 10.5 17.5Z"
+              fill="#FDFDFD"
+            />
+          </svg>
+        )}
         {data.title === "커뮤니티" ? (
           <CommunityNotification />
         ) : Boolean(data.travelHostUser) ? (
@@ -46,9 +62,12 @@ const NotificationItem = ({ data }: NotificationItemProps) => {
             </TimeContainer>
           </InfoContainer>
           <Content>{data.content}</Content>
+          {data?.title === "신고 접수 경고" && (
+            <InquiryButton onClick={() => router.push("/explanation")}>소명 사유 입력</InquiryButton>
+          )}
         </TextContainer>
       </TopContainer>
-      {data?.title !== "멤버 댓글 알림" && data?.title !== "커뮤니티" && (
+      {data?.title !== "멤버 댓글 알림" && data?.title !== "커뮤니티" && data?.title !== "신고 접수 경고" && (
         <TravelContainer>
           <Badge text="마감" backgroundColor={palette.keycolorBG} color={palette.keycolor} isDueDate={false} />
           <TravelTitle>{data.title}</TravelTitle>
@@ -110,6 +129,20 @@ const Content = styled.div`
   line-height: 22.4px;
 `;
 
+const InquiryButton = styled.button`
+  background-color: ${palette.비강조4};
+  border-radius: 40px;
+  border: none;
+  outline: none;
+  height: 42px;
+  width: 100%;
+  text-align: center;
+  padding: 12.5px 0;
+  line-height: 17px;
+  font-size: 14px;
+  font-weight: 600;
+  color: ${palette.비강조};
+`;
 const TravelContainer = styled.div`
   background-color: ${palette.검색창};
   padding: 16px;
