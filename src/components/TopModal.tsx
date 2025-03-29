a8137006d0b05c26d3ad43bab62c8b95669bf969
mayrang <pkss0626@naver.com>
2025-03-30 01:15:56 +0900

    Fix: 방식 바꿔보기3

--- src/components/TopModal.tsx
+++ src/components/TopModal.tsx
@@ -179,17 +179,17 @@ const TopModal = ({
     return () => window.removeEventListener("resize", handleResize);
   }, []);
   useEffect(() => {
-    console.log("children", childrenRef.current?.getBoundingClientRect());
+    console.log("children", contentRef.current?.getBoundingClientRect());
     setModalHeight(
-      childrenRef.current?.getBoundingClientRect().height ? childrenRef.current?.getBoundingClientRect().height + 48 : 0
+      contentRef.current?.getBoundingClientRect().height ? contentRef.current?.getBoundingClientRect().height + 48 : 0
     );
     setContentHeight(
-      childrenRef.current?.getBoundingClientRect().height ? childrenRef.current?.getBoundingClientRect().height + 48 : 0
+      contentRef.current?.getBoundingClientRect().height ? contentRef.current?.getBoundingClientRect().height + 48 : 0
     );
     onHeightChange(
-      childrenRef.current?.getBoundingClientRect().height ? childrenRef.current?.getBoundingClientRect().height + 48 : 0
+      contentRef.current?.getBoundingClientRect().height ? contentRef.current?.getBoundingClientRect().height + 48 : 0
     );
-  }, [childrenRef.current?.getBoundingClientRect().height]);
+  }, [contentRef.current?.getBoundingClientRect().height]);
   // 드래그 중이 아닐 때만 contentHeight 업데이트
   useEffect(() => {
     if (contentRef.current) {
