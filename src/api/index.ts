12976b9a2e5e3832c08b83ff4cc16e1d335991a4
mayrang <pkss0626@naver.com>
2025-04-02 12:51:22 +0900

    Fix: 401시 refresh 요청하도록 변경(테스트 필요)

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,11 +21,17 @@ interface ApiResponse<T> {
   success: T | null;
 }
 
-export const handleApiResponse = <T>(response: AxiosResponse<ApiResponse<T | null>>): T | null => {
+export const handleApiResponse = async <T>(
+  response: AxiosResponse<ApiResponse<T | null>>
+): Promise<T | null> => {
   if (response.data.resultType !== "SUCCESS") {
     throw new Error("API call failed: Unexpected resultType");
   }
-
+  if (response.status === 401) {
+    await axiosInstance.post("/api/token/refresh", {});
+    const retryResponse = await axiosInstance.request(response.config);
+    return handleApiResponse(retryResponse);
+  }
   if (response.data?.error != null) {
     throw new Error(response.data.error?.reason || "Unknown error occurred");
   }
